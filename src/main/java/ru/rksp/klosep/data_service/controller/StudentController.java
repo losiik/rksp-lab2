7a831e004dba292b6e46274c5c08db8b6dcfe28b
losiik <49565548+losiik@users.noreply.github.com>
2025-12-12 02:47:48 +0300

    update student controller

--- src/main/java/ru/rksp/klosep/data_service/controller/StudentController.java
+++ src/main/java/ru/rksp/klosep/data_service/controller/StudentController.java
@@ -1,4 +1,38 @@
 package ru.rksp.klosep.data_service.controller;
 
-public class StudentController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.rksp.klosep.data_service.data.Student;
+import ru.rksp.klosep.data_service.data.StudentRepository;
+import ru.rksp.klosep.dataservice.api.StudentDataApi;
+import ru.rksp.klosep.dataservice.model.StudentDataCreateRequest;
+import ru.rksp.klosep.dataservice.model.StudentDataResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController implements StudentDataApi {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
+        Student student = new Student();
+        student.setName(request.getFullName());
+        student.setPassport(request.getPassport());
+        studentRepository.save(student);
+        StudentDataResponse response = new StudentDataResponse();
+        response.setId(student.getId());
+        response.setFullName(student.getName());
+        response.setPassport(student.getPassport());
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+  /*@Override
+  public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
+
+    return ResponseEntity.status(200).body(response);
+  }*/
 }
+
